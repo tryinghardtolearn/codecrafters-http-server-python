f5748f785156eb08c209935124648ac6372876c7
guan wang <samuelwang222@gmail.com>
2024-07-20 15:21:18 -0400

    parse target URL and send appropriate response

--- app/main.py
+++ app/main.py
@@ -8,10 +8,15 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, addr  = server_socket.accept() # wait for client
     print(f"Connection from address: {addr} has been established.")
-    response = "HTTP/1.1 200 OK\r\n\r\n"
-    conn.sendall(response.encode('utf-8'))
-    conn.close()
+    response = b"HTTP/1.1 200 OK\r\n\r\n"
 
+    request_data = conn.recv(1024).decode('utf-8')
+    request_line = request_data.split("\r\n")[0]
+    if request_line.split(" ")[1] != '/':
+        response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+
+    conn.sendall(response)
+    conn.close()
 
 
 
