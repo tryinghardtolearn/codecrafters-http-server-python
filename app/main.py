39d84045ad6c216d10c7ea313ca71498247dc528
guan wang <samuelwang222@gmail.com>
2024-07-23 21:54:34 -0400

    parse the request and return the content in response

--- app/main.py
+++ app/main.py
@@ -8,12 +8,30 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, addr  = server_socket.accept() # wait for client
     print(f"Connection from address: {addr} has been established.")
-    response = b"HTTP/1.1 200 OK\r\n\r\n"
+    response_status_line = "HTTP/1.1 200 OK"
 
     request_data = conn.recv(1024).decode('utf-8')
     request_line = request_data.split("\r\n")[0]
-    if request_line.split(" ")[1] != '/':
-        response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+    request_url = request_line.split(" ")[1]
+    request_type = request_url.split("/")[1]
+    message_str = request_url.split("/")[-1]
+    print("message_str",message_str)
+
+    content_type = "Content-Type: text/plain"
+    content_length = f"Content-Length: {len(message_str)}"
+    new_line = "\r\n"
+
+    if not request_url.startswith("/") :
+        response = ("HTTP/1.1 404 Not Found" + 2*new_line).encode()
+    elif request_type == '':
+        response = (response_status_line + 2*new_line).encode()
+    elif request_type == "echo":
+        response_body = new_line.join([content_type, content_length])+new_line
+        response = new_line.join([response_status_line, response_body, message_str]).encode()
+    else:
+        response = ("HTTP/1.1 404 Not Found" + 2*new_line).encode()
+        
+
 
     conn.sendall(response)
     conn.close()
