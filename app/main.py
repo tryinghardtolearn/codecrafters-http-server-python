08cf7bb3652c97c9d13568ba32d1c55d19522728
guan wang <samuelwang222@gmail.com>
2024-07-27 10:15:56 -0400

    Parse request header agent

--- app/main.py
+++ app/main.py
@@ -9,25 +9,33 @@ def main():
     conn, addr  = server_socket.accept() # wait for client
     print(f"Connection from address: {addr} has been established.")
     response_status_line = "HTTP/1.1 200 OK"
+    content_type = "Content-Type: text/plain"
+    new_line = "\r\n"
 
+    # parse request data
     request_data = conn.recv(1024).decode('utf-8')
-    request_line = request_data.split("\r\n")[0]
+    
+    # parse request_line
+    request_line = request_data.split(new_line)[0]
+    # parse request url
     request_url = request_line.split(" ")[1]
     request_type = request_url.split("/")[1]
-    message_str = request_url.split("/")[-1]
-    print("message_str",message_str)
 
-    content_type = "Content-Type: text/plain"
-    content_length = f"Content-Length: {len(message_str)}"
-    new_line = "\r\n"
 
     if not request_url.startswith("/") :
         response = ("HTTP/1.1 404 Not Found" + 2*new_line).encode()
     elif request_type == '':
         response = (response_status_line + 2*new_line).encode()
     elif request_type == "echo":
+        message_str = request_url.split("/")[-1]
+        content_length = f"Content-Length: {len(message_str)}"
         response_body = new_line.join([content_type, content_length])+new_line
         response = new_line.join([response_status_line, response_body, message_str]).encode()
+    elif request_type == "user-agent":
+        request_header_agent = request_data.split(new_line)[2].split(": ")[1]
+        content_length = f"Content-Length: {len(request_header_agent)}"
+        response_body = new_line.join([content_type, content_length])+new_line
+        response = new_line.join([response_status_line, response_body, request_header_agent]).encode()
     else:
         response = ("HTTP/1.1 404 Not Found" + 2*new_line).encode()
         
